1fbea336aa2620786c2e398a83797754a384d45a
HaileleulGirma <haileleulgirma000@gmail.com>
2026-06-04 10:37:57 +0300

    update ReporterController with corresponding endpoints.

--- backend/src/main/java/com/ena/mam/reporter/ReporterController.java
+++ backend/src/main/java/com/ena/mam/reporter/ReporterController.java
@@ -3,9 +3,8 @@ package com.ena.mam.reporter;
 import com.ena.mam.dto.request.CreateReporterRequest;
 import com.ena.mam.dto.response.CreateReporterResponse;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ReporterController {
@@ -21,5 +20,22 @@ public class ReporterController {
     public CreateReporterResponse create(
             @Valid @RequestBody CreateReporterRequest request){
         return reporterService.create(request);
+    }
+
+    @PutMapping("/api/reporter/{reporterId}")
+    public CreateReporterResponse update(
+            @PathVariable Long reporterId,
+            @Valid @RequestBody CreateReporterRequest request){
+        return reporterService.update(reporterId, request);
+    }
+
+    @DeleteMapping("/api/reporter/{reporterId}")
+    public ResponseEntity<Void> delete(
+            @PathVariable Long reporterId
+    )
+    {
+        reporterService.delete(reporterId);
+
+        return ResponseEntity.noContent().build();
     }
 }
