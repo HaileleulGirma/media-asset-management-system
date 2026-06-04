743d1f444e7bc3926cf65a77b5e2a04aa860d17c
HaileleulGirma <haileleulgirma000@gmail.com>
2026-06-04 10:35:34 +0300

    Implement CRUD operations for Cameraman: add update and delete methods in CameramanService

--- backend/src/main/java/com/ena/mam/cameraman/CameramanService.java
+++ backend/src/main/java/com/ena/mam/cameraman/CameramanService.java
@@ -2,6 +2,7 @@ package com.ena.mam.cameraman;
 
 import com.ena.mam.dto.request.CreateCameramanRequest;
 import com.ena.mam.dto.response.CreateCameramanResponse;
+import com.ena.mam.exception.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,5 +19,19 @@ public class CameramanService {
         Cameraman cameraman = cameramanMapper.toCameraman(request);
         Cameraman savedCameraman = cameramanRepository.save(cameraman);
         return cameramanMapper.toResponse(savedCameraman);
+    }
+
+    public CreateCameramanResponse update(Long cameramanId, CreateCameramanRequest request){
+        Cameraman cameraman = cameramanRepository.findById(cameramanId).orElseThrow(() -> new ResourceNotFoundException("Cameraman with id %d not found.".formatted(cameramanId)));
+        cameraman.setCameramanName(request.cameramanName());
+        cameraman.setActive(request.isActive());
+
+        Cameraman savedCameraman = cameramanRepository.save(cameraman);
+
+        return cameramanMapper.toResponse(savedCameraman);
+    }
+
+    public void delete(Long cameramanId){
+        cameramanRepository.deleteById(cameramanId);
     }
 }
