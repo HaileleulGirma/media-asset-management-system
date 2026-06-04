bc18bb0a0c3d7ca04fcde8e7cc56392316712cf9
HaileleulGirma <haileleulgirma000@gmail.com>
2026-06-04 10:36:55 +0300

    Implement CRUD operations for Reporter: add update and delete methods in ReporterService

--- backend/src/main/java/com/ena/mam/reporter/ReporterService.java
+++ backend/src/main/java/com/ena/mam/reporter/ReporterService.java
@@ -2,6 +2,7 @@ package com.ena.mam.reporter;
 
 import com.ena.mam.dto.request.CreateReporterRequest;
 import com.ena.mam.dto.response.CreateReporterResponse;
+import com.ena.mam.exception.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -18,5 +19,21 @@ public class ReporterService {
         Reporter reporter = reporterMapper.toReporter(request);
         Reporter savedReporter = reporterRepository.save(reporter);
         return reporterMapper.toResponse(savedReporter);
+    }
+
+    public CreateReporterResponse update(Long id, CreateReporterRequest request){
+        Reporter reporter = reporterRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Reporter with id %d not found.".formatted(id)));
+
+        reporter.setReporterName(request.reporterName());
+        reporter.setActive(request.isActive());
+
+        Reporter savedReporter = reporterRepository.save(reporter);
+
+        return reporterMapper.toResponse(savedReporter);
+    }
+
+    public void delete(Long id){
+        reporterRepository.deleteById(id);
     }
 }
