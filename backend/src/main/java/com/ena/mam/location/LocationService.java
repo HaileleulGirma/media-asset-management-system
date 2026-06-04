460b497dd118a75d976c97a4fb891e83350114c6
HaileleulGirma <haileleulgirma000@gmail.com>
2026-06-04 10:36:10 +0300

    Implement CRUD operations for Location: add update and delete methods in LocationService

--- backend/src/main/java/com/ena/mam/location/LocationService.java
+++ backend/src/main/java/com/ena/mam/location/LocationService.java
@@ -2,6 +2,7 @@ package com.ena.mam.location;
 
 import com.ena.mam.dto.request.CreateLocationRequest;
 import com.ena.mam.dto.response.CreateLocationResponse;
+import com.ena.mam.exception.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,5 +20,19 @@ public class LocationService {
         Location savedLocation = locationRepository.save(location);
 
         return locationMapper.toResponse(savedLocation);
+    }
+
+    public CreateLocationResponse update(Long id, CreateLocationRequest request){
+        Location location = locationRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Location with id %d not found.".formatted(id)));
+        location.setLocationName(request.locationName());
+        location.setAbroad(request.isAbroad());
+
+        Location savedLocation = locationRepository.save(location);
+
+        return locationMapper.toResponse(savedLocation);
+    }
+
+    public void delete(Long id){
+        locationRepository.deleteById(id);
     }
 }
