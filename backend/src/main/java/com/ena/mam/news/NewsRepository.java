b9d7e4cb53a9c1d372676d13a932f0eeb982ec44
HaileleulGirma <haileleulgirma000@gmail.com>
2026-06-11 10:05:51 +0300

    Enhance NewsRepository to support JpaSpecificationExecutor

--- backend/src/main/java/com/ena/mam/news/NewsRepository.java
+++ backend/src/main/java/com/ena/mam/news/NewsRepository.java
@@ -1,6 +1,11 @@
 package com.ena.mam.news;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface NewsRepository extends JpaRepository<News, Long> {
+public interface NewsRepository extends
+        JpaRepository<News, Long>,
+        JpaSpecificationExecutor<News>,
+        NewsRepositoryCustom
+{
 }
