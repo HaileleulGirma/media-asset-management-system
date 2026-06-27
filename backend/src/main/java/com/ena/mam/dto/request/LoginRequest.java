3725c0f09853f8d648c83cc6bd178c24f8ba0ea6
HaileleulGirma <haileleulgirma000@gmail.com>
2026-06-28 02:36:45 +0300

    Add LoginRequest and AuthResponse DTOs for authentication handling

---
+++ backend/src/main/java/com/ena/mam/dto/request/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.ena.mam.dto.request;
+
+
+public record LoginRequest(String username, String password) {}
---
+++ backend/src/main/java/com/ena/mam/dto/response/AuthResponse.java
@@ -0,0 +1,3 @@
+package com.ena.mam.dto.response;
+
+public record AuthResponse(String token) {}
