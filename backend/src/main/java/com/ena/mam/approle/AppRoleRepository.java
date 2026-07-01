bc208ce230423f15893392ffce6528a4e554623b
HaileleulGirma <haileleulgirma000@gmail.com>
2026-07-01 07:48:01 +0300

    Add findByRoleName method to AppRoleRepository for role retrieval

--- backend/src/main/java/com/ena/mam/approle/AppRoleRepository.java
+++ backend/src/main/java/com/ena/mam/approle/AppRoleRepository.java
@@ -2,5 +2,8 @@ package com.ena.mam.approle;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AppRoleRepository extends JpaRepository<AppRole, Long> {
+    Optional<AppRole> findByRoleName(String roleName);
 }
